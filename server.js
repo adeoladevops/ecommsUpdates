8d973ab6113ce2cd1634500bf5b20e0d18ca0b55
adeoladevops <iloriabdulqoyum@gmail.com>
2024-11-29 02:30:53 +0100

    server.js addition

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve the index.html file for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Mock data storage
 let products = [
     { name: 'Smartwatch A', price: 150 },
